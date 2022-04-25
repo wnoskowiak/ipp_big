1b024fdba0b95aac4af589652301a04fc08e2986
wojtek <a@dupa.gov>
2022-04-25 13:44:01 +0200

    pierwsza wersja gotowa do debugu

--- src/phone_forward.c
+++ src/phone_forward.c
@@ -1,12 +1,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
-
-/*
-
-SPRAWDZAĆ CZY CHARY REPREZENTUJĄ LICZBY!!!!
-
-*/
+#include <string.h>
+#include <limits.h>
 
 struct PhoneForward;
 
@@ -25,6 +21,12 @@ struct PhoneNumbers
 };
 typedef struct PhoneNumbers PhoneNumbers;
 
+typedef struct phfwdGet_helper
+{
+    size_t depth;
+    char *prefix;
+} phfwdGet_helper_t;
+
 PhoneForward *phfwdNew(void)
 {
     return (PhoneForward *)malloc(sizeof(PhoneForward));
@@ -32,43 +34,65 @@ PhoneForward *phfwdNew(void)
 
 void phfwdDelete(PhoneForward *pf)
 {
-    PhoneForward *temp = *pf->further;
-    for (size_t i = 0; i < 9; i++)
+    if (pf)
     {
-        if (temp)
+        for (size_t i = 0; i < 10; i++)
         {
-            phfwdDelete(temp);
+            if (pf->further[i])
+            {
+                phfwdDelete(pf->further[i]);
+            }
         }
-        temp++;
+        if (pf->redirect)
+        {
+            free(pf->redirect);
+        }
+        free(pf);
     }
-    free(pf);
 }
 
 bool phfwdAdd(PhoneForward *pf, char const *num1, char const *num2)
 {
-    bool different = false;
-    bool length_same = false;
-    size_t length = 0;
-    while (length < 10)
+    bool works= false, different= false, num1_end= false, num2_end = false;
+    size_t length = 0, len1 = 0, len2 = 0;
+    while (length < ULONG_MAX)
     {
-        if (num1[length] == '\0')
+        if (num1_end & num2_end)
         {
-            if (num2[length] == '\0')
-            {
-                length_same = true;
-            }
+            works = true;
             break;
         }
-        if (num1[length] != num2[length])
+        if (num1[length] == '\0')
         {
-            different = true;
+            len1 = length;
+            num1_end = true;
+        }
+        if (num2[length] == '\0')
+        {
+            len2 = length;
+            num2_end = true;
+        }
+        if (
+            ((!num1_end) & (num1[length] - '0' < 0 || num1[length] - '0' > 9)) ||
+            ((!num2_end) & (num2[length] - '0' < 0 || num2[length] - '0' > 9)))
+        {
+            break;
+        }
+        if (!different)
+        {
+            if (num1[length] != num2[length])
+            {
+                different = true;
+            }
         }
         length++;
     }
-    if (different & length_same)
+    if (different & works)
     {
+        bool mem_fail = false;
+        PhoneForward *first_created = NULL;
         PhoneForward *proper_place = pf;
-        for (size_t i = 0; i < length; i++)
+        for (size_t i = 0; i < len1; i++)
         {
             PhoneForward *temp = proper_place->further[(size_t)(num1[i] - '0')];
             if (temp)
@@ -77,15 +101,35 @@ bool phfwdAdd(PhoneForward *pf, char const *num1, char const *num2)
             }
             else
             {
-                // dodać fail w przypadku niezalokowania pamięci
                 temp = phfwdNew();
-                proper_place->further[(size_t)(num1[i] - '0')] = temp;
-                proper_place = temp;
+                if (temp)
+                {
+                    if (!first_created)
+                    {
+                        first_created = temp;
+                    }
+                    proper_place->further[(size_t)(num1[i] - '0')] = temp;
+                    proper_place = temp;
+                }
+                else
+                {
+                    mem_fail = true;
+                    break;
+                }
             }
         }
-        // tu jednak trzeba kurwa kopiować
-        proper_place->redirect = num2;
-        return true;
+        if (!mem_fail)
+        {
+            proper_place->redirect = (char *)malloc((len2 + 1) * sizeof(char));
+            strcpy(proper_place->redirect, num2);
+            proper_place->redirect[len2] = '\0';
+            return true;
+        }
+        else
+        {
+            phfwdDelete(first_created);
+            return false;
+        }
     }
     else
     {
@@ -95,94 +139,167 @@ bool phfwdAdd(PhoneForward *pf, char const *num1, char const *num2)
 
 void phfwdRemove(PhoneForward *pf, char const *num)
 {
-    bool found = false;
+    bool works = true;
     PhoneForward *proper_place = pf;
+    PhoneForward *deepest_empty = NULL;
     size_t i = 0;
-    while (i < 9)
+    while (i < ULONG_MAX)
     {
         if (num[i] == '\0')
         {
-            found = true;
+            break;
+        }
+        if ((num[i] - '0' < 0 || num[i] - '0' > 9))
+        {
+            works = false;
             break;
         }
         PhoneForward *temp = proper_place->further[(size_t)(num[i] - '0')];
         if (temp)
         {
+            if (!deepest_empty)
+            {
+                deepest_empty = temp;
+            }
+            for (size_t i = 0; i < 10; i++)
+            {
+                if (proper_place->further[i])
+                {
+                    deepest_empty = NULL;
+                    break;
+                }
+            }
             proper_place = temp;
         }
         else
         {
+            works = false;
             break;
         }
         i++;
     }
-    proper_place->redirect = NULL;
-    bool empty = true;
-    for (size_t i = 0; i < 9; i++)
+    if (works)
     {
-        if (proper_place->further[i])
+        if (deepest_empty)
         {
-            empty = false;
-            break;
+            phfwdDelete(deepest_empty);
         }
-    }
-    if (empty)
-    {
-        phfwdDelete(proper_place);
+        else
+        {
+            phfwdDelete(proper_place);
+        }
     }
 }
 
 PhoneNumbers *phfwdGet(PhoneForward const *pf, char const *num)
 {
     PhoneNumbers *result = (PhoneNumbers *)malloc(sizeof(PhoneNumbers));
-    size_t temp = 0;
-    bool num_ok = false;
-    while (temp < 10)
+    if (result)
     {
-        if (num[temp] == '\0')
-        {
-            if (temp == 9)
-            {
-                num_ok = true;
-            }
-            break;
-        }
-    }
-    if (num_ok)
-    {
-        char* redirected = (char*)malloc(8*sizeof(char));
-        PhoneForward *proper_place = pf;
+        bool reached_end= false , found_deepest = false;
+        phfwdGet_helper_t deepest_redirect;
+        deepest_redirect.depth = 0;
+        PhoneForward const *proper_place = pf;
         size_t i = 0;
-        while (i < 9)
+        while (i < ULONG_MAX)
         {
             if (num[i] == '\0')
             {
+                reached_end = true;
                 break;
             }
-            PhoneForward *temp = proper_place->further[(size_t)(num[i] - '0')];
-            if (temp)
-            {
-                proper_place = temp;
-            }
-            else
+            if ((num[i] - '0' < 0 || num[i] - '0' > 9))
             {
                 break;
+            }
+            if (!found_deepest)
+            {
+                if (proper_place->redirect)
+                {
+                    deepest_redirect.depth = i;
+                    deepest_redirect.prefix = proper_place->redirect;
+                }
+                PhoneForward *temp = proper_place->further[(size_t)(num[i] - '0')];
+                if (temp)
+                {
+                    proper_place = temp;
+                }
+                else
+                {
+                    found_deepest = true;
+                    break;
+                }
             }
             i++;
         }
-        if (proper_place->redirect)
+        if (reached_end)
         {
-            // tutaj musisz konkatonować listy charów
+            char *new_number;
+            if (deepest_redirect.prefix)
+            {
+                size_t new_length = strlen(deepest_redirect.prefix) + i - deepest_redirect.depth;
+                new_number = (char *)malloc(new_length * sizeof(char));
+                if (new_number)
+                {
+                    strcpy(new_number, deepest_redirect.prefix);
+                    strcpy(new_number, &num[i + 1]);
+                    new_number[new_length - 1] = '\0';
+                }
+                else
+                {
+                    result = NULL;
+                }
+            }
+            else
+            {
+                new_number = (char *)malloc((i + 1) * sizeof(char));
+                if (new_number)
+                {
+                    strcpy(new_number, num);
+                    new_number[i - 1] = '\0';
+                }
+                else
+                {
+                    result = NULL;
+                }
+            }
+            if (new_number)
+            {
+                result->list = (char **)malloc(sizeof(char *));
+                if (result->list)
+                {
+                    result->len = 1;
+                    result->list[0] = new_number;
+                }
+                else
+                {
+                    result = NULL;
+                }
+            }
         }
         else
         {
-            result->len = 1;
-            result->list = (char **)malloc(sizeof(char));
+            result->len = 0;
         }
     }
-    else
+    return result;
+}
+
+char const *phnumGet(PhoneNumbers const *pnum, size_t idx)
+{
+    if (pnum->len >= idx)
     {
-        result->len = 0;
-        return result;
+        return NULL;
     }
+    return pnum->list[idx];
+}
+
+void phnumDelete(PhoneNumbers *pnum)
+{
+    for (size_t i = 0; i < pnum->len; i++)
+    {
+        free(pnum->list[i]);
+    }
+    free(pnum->list);
+    free(pnum);
 }
--- src/phone_forward_example.c
+++ src/phone_forward_example.c
@@ -55,89 +55,89 @@ int main() {
   assert(strcmp(phnumGet(pnum, 0), "433") == 0);
   phnumDelete(pnum);
 
-  pnum = phfwdReverse(pf, "432");
-  assert(strcmp(phnumGet(pnum, 0), "431") == 0);
-  assert(strcmp(phnumGet(pnum, 1), "432") == 0);
-  assert(phnumGet(pnum, 2) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "432");
+//   assert(strcmp(phnumGet(pnum, 0), "431") == 0);
+//   assert(strcmp(phnumGet(pnum, 1), "432") == 0);
+//   assert(phnumGet(pnum, 2) == NULL);
+//   phnumDelete(pnum);
 
-  pnum = phfwdReverse(pf, "433");
-  assert(strcmp(phnumGet(pnum, 0), "432") == 0);
-  assert(strcmp(phnumGet(pnum, 1), "433") == 0);
-  assert(phnumGet(pnum, 2) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "433");
+//   assert(strcmp(phnumGet(pnum, 0), "432") == 0);
+//   assert(strcmp(phnumGet(pnum, 1), "433") == 0);
+//   assert(phnumGet(pnum, 2) == NULL);
+//   phnumDelete(pnum);
 
-  pnum = phfwdReverse(pf, "987654321");
-  assert(strcmp(phnumGet(pnum, 0), "12387654321") == 0);
-  assert(strcmp(phnumGet(pnum, 1), "987654321") == 0);
-  assert(phnumGet(pnum, 2) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "987654321");
+//   assert(strcmp(phnumGet(pnum, 0), "12387654321") == 0);
+//   assert(strcmp(phnumGet(pnum, 1), "987654321") == 0);
+//   assert(phnumGet(pnum, 2) == NULL);
+//   phnumDelete(pnum);
 
-  phfwdRemove(pf, "12");
+//   phfwdRemove(pf, "12");
 
-  pnum = phfwdGet(pf, "123456");
-  assert(strcmp(phnumGet(pnum, 0), "123456") == 0);
-  phnumDelete(pnum);
+//   pnum = phfwdGet(pf, "123456");
+//   assert(strcmp(phnumGet(pnum, 0), "123456") == 0);
+//   phnumDelete(pnum);
 
-  pnum = phfwdReverse(pf, "987654321");
-  assert(strcmp(phnumGet(pnum, 0), "987654321") == 0);
-  assert(phnumGet(pnum, 1) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "987654321");
+//   assert(strcmp(phnumGet(pnum, 0), "987654321") == 0);
+//   assert(phnumGet(pnum, 1) == NULL);
+//   phnumDelete(pnum);
 
-  assert(phfwdAdd(pf, "567", "0") == true);
-  assert(phfwdAdd(pf, "5678", "08") == true);
+//   assert(phfwdAdd(pf, "567", "0") == true);
+//   assert(phfwdAdd(pf, "5678", "08") == true);
 
-  pnum = phfwdReverse(pf, "08");
-  assert(strcmp(phnumGet(pnum, 0), "08") == 0);
-  assert(strcmp(phnumGet(pnum, 1), "5678") == 0);
-  assert(phnumGet(pnum, 2) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "08");
+//   assert(strcmp(phnumGet(pnum, 0), "08") == 0);
+//   assert(strcmp(phnumGet(pnum, 1), "5678") == 0);
+//   assert(phnumGet(pnum, 2) == NULL);
+//   phnumDelete(pnum);
 
-  assert(phfwdAdd(pf, "A", "1") == false);
-  assert(phfwdAdd(pf, "1", "A") == false);
+//   assert(phfwdAdd(pf, "A", "1") == false);
+//   assert(phfwdAdd(pf, "1", "A") == false);
 
-  phfwdRemove(pf, "");
-  phfwdRemove(pf, NULL);
+//   phfwdRemove(pf, "");
+//   phfwdRemove(pf, NULL);
 
-  pnum = phfwdGet(pf, "A");
-  assert(phnumGet(pnum, 0) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdGet(pf, "A");
+//   assert(phnumGet(pnum, 0) == NULL);
+//   phnumDelete(pnum);
 
-  pnum = phfwdReverse(pf, "A");
-  assert(phnumGet(pnum, 0) == NULL);
-  phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "A");
+//   assert(phnumGet(pnum, 0) == NULL);
+//   phnumDelete(pnum);
 
-  phfwdAdd(pf, "12", "123");
-  pnum = phfwdGet(pf, "123");
-  assert(strcmp(phnumGet(pnum, 0), "1233") == 0);
-  phnumDelete(pnum);
+//   phfwdAdd(pf, "12", "123");
+//   pnum = phfwdGet(pf, "123");
+//   assert(strcmp(phnumGet(pnum, 0), "1233") == 0);
+//   phnumDelete(pnum);
 
-  phfwdAdd(pf, "2", "4");
-  phfwdAdd(pf, "23", "4");
-  pnum = phfwdReverse(pf, "434");
-  assert(strcmp(phnumGet(pnum, 0), "2334") == 0);
-  assert(strcmp(phnumGet(pnum, 1), "234") == 0);
-  assert(strcmp(phnumGet(pnum, 2), "434") == 0);
-  assert(phnumGet(pnum, 3) == NULL);
-  phnumDelete(pnum);
+//   phfwdAdd(pf, "2", "4");
+//   phfwdAdd(pf, "23", "4");
+//   pnum = phfwdReverse(pf, "434");
+//   assert(strcmp(phnumGet(pnum, 0), "2334") == 0);
+//   assert(strcmp(phnumGet(pnum, 1), "234") == 0);
+//   assert(strcmp(phnumGet(pnum, 2), "434") == 0);
+//   assert(phnumGet(pnum, 3) == NULL);
+//   phnumDelete(pnum);
 
-  phfwdDelete(pf);
-  pnum = NULL;
-  phnumDelete(pnum);
-  pf = NULL;
-  phfwdDelete(pf);
+//   phfwdDelete(pf);
+//   pnum = NULL;
+//   phnumDelete(pnum);
+//   pf = NULL;
+//   phfwdDelete(pf);
 
-  pf = phfwdNew();
-  phfwdAdd(pf, "1234", "76");
-  pnum = phfwdGet(pf, "1234581");
-  assert(strcmp(phnumGet(pnum, 0), "76581") == 0);
-  phnumDelete(pnum);
-  pnum = phfwdGet(pf, "7581");
-  assert(strcmp(phnumGet(pnum, 0), "7581") == 0);
-  phnumDelete(pnum);
-  pnum = phfwdReverse(pf, "7581");
-  assert(strcmp(phnumGet(pnum, 0), "7581") == 0);
-  assert(phnumGet(pnum, 1) == NULL);
-  phnumDelete(pnum);
-  phfwdDelete(pf);
+//   pf = phfwdNew();
+//   phfwdAdd(pf, "1234", "76");
+//   pnum = phfwdGet(pf, "1234581");
+//   assert(strcmp(phnumGet(pnum, 0), "76581") == 0);
+//   phnumDelete(pnum);
+//   pnum = phfwdGet(pf, "7581");
+//   assert(strcmp(phnumGet(pnum, 0), "7581") == 0);
+//   phnumDelete(pnum);
+//   pnum = phfwdReverse(pf, "7581");
+//   assert(strcmp(phnumGet(pnum, 0), "7581") == 0);
+//   assert(phnumGet(pnum, 1) == NULL);
+//   phnumDelete(pnum);
+//   phfwdDelete(pf);
 }
